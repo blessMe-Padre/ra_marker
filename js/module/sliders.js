4c8effa1fa1215d33e3c0dc446dbc07786b913c8
Сергей Субботин <zarodiny@yandex.ru>
2024-03-11 12:48:47 +1000

    feat: добавляет секцию new

--- js/module/sliders.js
+++ js/module/sliders.js
@@ -31,4 +31,36 @@ export const initSlider = () => {
             },
         });
     }
+
+    // слайдер "Новые категории"
+    const newSlider = document.querySelector('.new-swiper');
+    if (newSlider) {
+        const item = new Swiper('.new-swiper', {
+            loop: true,
+            spaceBetween: 30,
+            breakpoints: {
+                374: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                },
+                767: {
+                    slidesPerView: 2,
+                },
+                1023: {
+                    slidesPerView: 4,
+                    spaceBetween: 30,
+                },
+            },
+
+            navigation: {
+                nextEl: '.swiper-button-next--new',
+                prevEl: '.swiper-button-prev--new',
+            },
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true
+            },
+        });
+    }
 }
